09ae2e108a4d3f820fbd4a268ce1947c6a679b01
TARGE-HUSEYIN <TARGE-HUSEYIN@DESKTOP-L69KJ98>
2022-12-06 15:23:43 +0300

    message updated

--- src/Core/OnionArchDemo.Application/Wrappers/ServiceResponse.cs
+++ src/Core/OnionArchDemo.Application/Wrappers/ServiceResponse.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace OnionArchDemo.Application.Wrappers
 {
-    public class ServiceResponse<T>
+    public class ServiceResponse<T>:BaseResponse
     {
         public T Value { get; set; }
 
