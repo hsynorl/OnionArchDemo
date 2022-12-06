848d83bac038dd33e15741e5b2418d8f1e09bba7
TARGE-HUSEYIN <TARGE-HUSEYIN@DESKTOP-L69KJ98>
2022-12-06 10:49:41 +0300

    Add project files.

---
+++ src/Core/OnionArchDemo.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchDemo.Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException():this("Validaton error occured") { }
+
+        public ValidationException(String Message):base(Message)
+        {
+
+        }
+        public ValidationException(Exception ex):this(ex.Message) { }
+        
+    
+    }
+}
---
+++ src/Core/OnionArchDemo.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using OnionArchDemo.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchDemo.Domain.Entities
+{
+    public class Product:BaseEntity
+    {
+        public String Name { get; set; }
+        public decimal  Value { get; set; }
+
+        public int Quentity { get; set; }
+    }
+}
